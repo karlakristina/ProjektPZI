061c5227218134f3e9649b50b5447778677897a8
karlakristina <karla.kristina.skegro@fpmoz.sum.ba>
2022-03-08 16:23:57 +0100

    prvi commit

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+   
+});
+
+Route::get('/vizija', function () {
+    return view('vizija');
+   
 });
 
 Auth::routes();
